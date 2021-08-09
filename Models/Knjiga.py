2dd2f7cb02e645850d3f237fcda546ae63756774
Ognjen NRT-26/18 <ognjennrt2618@gs.viser.edu.rs>
2021-08-09 17:00:21 +0200

    Initial commit

---
+++ Models/Knjiga.py
@@ -0,0 +1,8 @@
+class Knjiga:
+    def __init__(self, naslov, indeks, autor):
+        self.naslov = naslov
+        self.indeks = indeks
+        self.autor = autor
+
+    def __str__(self) -> str:
+        return self.naslov + " " + self.autor + " ID:" + self.indeks
