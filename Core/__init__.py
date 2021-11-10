ba22d305fadad03ab2e93fb062cf27553dea7905
Ognjen NRT-26/18 <ognjennrt2618@gs.viser.edu.rs>
2021-11-10 16:22:06 +0100

    Added starting files for client and server operations

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,6 +1,11 @@
 from .DataGridView import *
-from .Operations import *
 from .UserFrames import *
 from .AdminFrames import *
 from .Exceptions import *
 from .OperatorFrames import *
+# Standalone app
+# from .Operations import *
+# Client
+from ClientOperations import *
+# Server
+from ServerOperations import *
